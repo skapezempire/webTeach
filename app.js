8b9b597bc5d24628d3b2d35023e1a98c4ac98d82
skapezempire <skapezempire@gmail.com>
2022-07-26 10:42:37 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector('nav ul')
+let hamburger = document.querySelector('nav .humburger')
+hamburger.addEventListener('click', function(){
+    menu.classList.toggle('show')
+
+}) 
